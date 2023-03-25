8eec33d965a1d2d05f00efb542e45595f8f4501d
Abriial <codjumper@mail.ru>
2023-03-25 17:10:24 +0300

    Annotation Config

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,5 +1,7 @@
 package ru.netology.servlet;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.netology.config.Config;
 import ru.netology.controller.PostController;
 import ru.netology.repository.PostRepository;
 import ru.netology.service.PostService;
@@ -16,9 +18,12 @@ public class MainServlet extends HttpServlet {
 
   @Override
   public void init() {
-    final var repository = new PostRepository();
-    final var service = new PostService(repository);
-    controller = new PostController(service);
+//    final var repository = new PostRepository();
+//    final var service = new PostService(repository);
+//    controller = new PostController(service);
+
+    AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+    controller = (PostController) context.getBean("postController");
   }
 
   @Override
